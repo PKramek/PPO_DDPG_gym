4d7fef53eea287a6139d10c5c14df9864f35e7b7
Piotr Kramek <peterkramek@gmail.com>
2021-01-26 18:27:02 +0100

    Update PPO and add trained models

--- util.py
+++ util.py
@@ -8,11 +8,11 @@ horizon_len = 4000
 epochs = 250
 gamma = 0.99
 epsilon = 0.2
-lambda_ = 0.95
+lambda_ = 0.97
 actor_learning_rate = 1e-3
 critic_learning_rate = 1e-3
-train_actor_iterations = 80
-train_critic_iterations = 80
+train_actor_iterations = 110
+train_critic_iterations = 110
 minibatch_size = 64
 hidden_size = 64
 
